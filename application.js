9be69cc42c147f1de89e23089ec1fb955fae2821
Javier Collado <javier.collado@gmail.com>
2013-10-09 11:09:00 +0200

    Added code to set weekday string

--- application.js
+++ application.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    // Set weekday string in the upper left corner
+    var weekdays = [
+        "Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday",
+    ];
+    var date = new Date();
+    var day = weekdays[date.getDay()];
+
+    $("#weekday").text(day);
+
+    // Make sure headers and arrows are updated on hover
     $("#section_basecamp").hover(function() {
         $("#header_default, #header_basecamp, #arrow_basecamp").toggle();
     });
