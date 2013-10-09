22bf03c3aa11e86433cfdae4c06037894fd59e2f
Javier Collado <javier.collado@gmail.com>
2013-10-09 10:50:45 +0200

    Added arrows and javascript code to show/hide on hover

--- application.js
+++ application.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    console.log('hello from jquery!')
+    $("#section_basecamp").hover(function() {
+        $("#header_default, #header_basecamp, #arrow_basecamp").toggle();
+    });
+
+    $("#section_highrise").hover(function() {
+        $("#header_default, #header_highrise, #arrow_highrise").toggle();
+    });
+
+    $("#section_campfire").hover(function() {
+        $("#header_default, #header_campfire, #arrow_campfire").toggle();
+    });
 });
